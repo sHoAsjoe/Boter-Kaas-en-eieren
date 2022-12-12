65131d81b4c6f5a9d63c94a8bc6fb434826213e5
sHoAsjoe <302926554@student.rocmondriaan.nl>
2022-12-12 12:26:36 +0100

    first changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,60 @@
 console.log ('hello world');
+
+let  cells = document.querySelectorAll('.cell');
+// console.log (cells);
+let playersMessage = document.querySelector('.playerMessage');
+// console.log (playersMessage);
+const restaBtn = document.querySelector('resetbutton');
+
+const winCondition = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4 ,7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 8],
+];
+// speler X is eerste
+let xPlayer = 'true';
+let count = 0;
+
+// spel logica 
+
+
+// vak toont X of O bij klikken 
+
+cells.forEach((element) => {
+     element.addEventListener("click", () => {
+        if (xPlayer) {
+            xPlayer = false;
+            //laat X zien op het bord
+            element.innerText = 'X';
+            element.disabled = true;
+        } else {
+            xPlayer = true;
+            //
+            element.innerText = 'O';
+            element.disabled = true;
+        }
+        // verhoog met 1 bij elke klik
+        count += 1;
+        if (count === 9){
+            //Het is gelijk spel
+        }
+        // check winner 
+        // winChecker (){
+
+        // }
+     });
+});
+
+// loop through playfield
+
+
+
+// functie check winnen
+function checkWinner () {
+
+}
