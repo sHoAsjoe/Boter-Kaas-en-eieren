9ffa5013040460c8e79b6d5648434ed98bb31210
sHoAsjoe <302926554@student.rocmondriaan.nl>
2023-01-23 12:51:23 +0100

    commentaar toegevoegd aan for loop & functions.

--- js/main.js
+++ js/main.js
@@ -12,13 +12,16 @@ const winningCombinations = [
   [0, 4, 8], 
   [2, 4, 6]];
 
+
+
+  // for loop die door de cellen heen loopt //
 for (let i = 0; i < cells.length; i++) {
-  cells[i].addEventListener('click', function() {
-    if (board[i] === '') {
-      board[i] = player;
+  cells[i].addEventListener('click', function() { // event listener die kijkt of er op een cel geklikt wordt //
+    if (board[i] === '') { // als de cel leeg is, dan wordt er een X of O geplaatst //
+      board[i] = player; // de waarde van de cel wordt gelijk gesteld aan de waarde van de speler //
       this.innerHTML = player;
-      checkForWinner();
-      switchPlayer();
+      checkForWinner();// als er een X of O geplaatst is, dan wordt er gecheckt of er een winnaar is //
+      switchPlayer();// als er op een cel geklikt wordt, dan wordt de speler gewisseld //
     } else {
       alert('This spot is already taken. Please choose another one.');
     }
@@ -26,7 +29,7 @@ for (let i = 0; i < cells.length; i++) {
 }
 
 function switchPlayer() {
-  if (player === 'X') {
+  if (player == 'X') { 
     player = 'O';
   } else {
     player = 'X';
@@ -35,7 +38,7 @@ function switchPlayer() {
 
 function checkForWinner() {
     for (let i = 0; i < winningCombinations.length; i++) {
-    if (board[winningCombinations[i][0]] === player && board[winningCombinations[i][1]] === player && board[winningCombinations[i][2]] === player) {
+    if (board[winningCombinations[i][0]] == player && board[winningCombinations[i][1]] == player && board[winningCombinations[i][2]] == player) {
       alert(player + ' wins!');
     }
   }
